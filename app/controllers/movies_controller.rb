b5016dd48797262286cb9fcb3f77bfc145783564
john-roufaeil <john.f.roufaeil@gmail.com>
2026-06-19 15:12:12 +0300

    feat: add movies controller and routes

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,43 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: 'Movie created'
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie updated"
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, notice: "Movie deleted"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :body)
+  end
+end
